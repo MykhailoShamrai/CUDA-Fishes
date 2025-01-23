e5fc4d77280ad1bde712a15b961990ae958f165b
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-01-24 00:50:30 +0100

    added changing screen widht and height, also added new buttons

--- src/main.cu
+++ src/main.cu
@@ -13,14 +13,16 @@
 #include "../main_loop/main_loop_cpu.cuh"
 #include <vector>
 
-#define NUMBER_OF_FISHES 2000
 #define WIDTH 1600
 #define HEIGHT 900
+static int screen_width = WIDTH;
+static int screen_height = HEIGHT;
 
 #define NUMBER_OF_POINTS_FOR_CIRCLE 32
 
 #define THREAD_NUMBER 128
 
+static int numberOfFishes = 10000;
 float cursorPosX = 0.0f;
 float cursorPosY = 0.0f;
 
@@ -70,6 +72,13 @@ static void cursor_position_callback(GLFWwindow* window, double xpos, double ypo
 	cursorPosY = -(float)ypos + HEIGHT / 2;
 }
 
+static void framebuffer_size_callback(GLFWwindow*, int new_width, int new_height)
+{
+	screen_width = new_width;
+	screen_height = new_height;
+	glViewport(0, 0, new_width, new_height);
+}
+
 int main()
 {
 	// Initialization
@@ -92,6 +101,7 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_press_callback);
 	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -108,13 +118,10 @@ int main()
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init();
 
-
-
-	Fishes h_fishes = Fishes(NUMBER_OF_FISHES, false);
-	//h_fishes.GenerateTestFishes();
+	Fishes h_fishes = Fishes(numberOfFishes, false);
 	h_fishes.GenerateRandomFishes(WIDTH, HEIGHT, 1.0f, 4.0f);
 
-	Fishes d_fishes = Fishes(NUMBER_OF_FISHES, true);
+	Fishes d_fishes = Fishes(numberOfFishes, true);
 	d_fishes.d_CopyFishesFromCPU(h_fishes);
 
 
@@ -168,7 +175,7 @@ int main()
 	glGenVertexArrays(1, &VAO_Triangles_GPU);
 	glBindVertexArray(VAO_Triangles_GPU);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_Triangles_GPU);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * NUMBER_OF_FISHES, nullptr, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * numberOfFishes, nullptr, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	
@@ -180,31 +187,31 @@ int main()
 	glGenVertexArrays(1, &VAO_Circles_GPU);
 	glBindVertexArray(VAO_Circles_GPU);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_Circles_GPU);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * NUMBER_OF_FISHES, nullptr, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * numberOfFishes, nullptr, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	cudaGraphicsResource* cuda_vbo_res_circle;
 	checkCudaErrors(cudaGraphicsGLRegisterBuffer(&cuda_vbo_res_circle, VBO_Circles_GPU, cudaGraphicsRegisterFlagsWriteDiscard));
 	
-	GLint firsts[NUMBER_OF_FISHES];
-	GLsizei count[NUMBER_OF_FISHES];
+	GLint* firsts = (GLint*)malloc(sizeof(GLint) * numberOfFishes);
+	GLsizei* count = (GLsizei*)malloc(sizeof(GLsizei) * numberOfFishes);
 
-	for (int i = 0; i < NUMBER_OF_FISHES; i++)
+	for (int i = 0; i < numberOfFishes; i++)
 	{
 		firsts[i] = i * NUMBER_OF_POINTS_FOR_CIRCLE;
 		count[i] = NUMBER_OF_POINTS_FOR_CIRCLE;
 	}
 
-	float* h_triangles_buffer = (float*)malloc(sizeof(float) * 6 * NUMBER_OF_FISHES);
-	float* h_circles_buffer = (float*)malloc(sizeof(float) * 2 * NUMBER_OF_POINTS_FOR_CIRCLE * NUMBER_OF_FISHES);
+	float* h_triangles_buffer = (float*)malloc(sizeof(float) * 6 * numberOfFishes);
+	float* h_circles_buffer = (float*)malloc(sizeof(float) * 2 * NUMBER_OF_POINTS_FOR_CIRCLE * numberOfFishes);
 
 	GLuint VBO_Triangles_CPU, VAO_Triangles_CPU;
 	glGenBuffers(1, &VBO_Triangles_CPU);
 	glGenVertexArrays(1, &VAO_Triangles_CPU);
 	glBindVertexArray(VAO_Triangles_CPU);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_Triangles_CPU);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * NUMBER_OF_FISHES, nullptr, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * numberOfFishes, nullptr, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -213,7 +220,7 @@ int main()
 	glGenVertexArrays(1, &VAO_Circles_CPU);
 	glBindVertexArray(VAO_Circles_CPU);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_Circles_CPU);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * NUMBER_OF_FISHES, h_circles_buffer, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * numberOfFishes, h_circles_buffer, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -222,14 +229,14 @@ int main()
 	checkCudaErrors(cudaMalloc((void**)&d_options, sizeof(Options)));
 	checkCudaErrors(cudaMemcpy(d_options, &h_options, sizeof(Options), cudaMemcpyHostToDevice));
 
-	Grid h_grid = Grid(NUMBER_OF_FISHES, h_options.radiusForFishes, WIDTH, HEIGHT, false);
-	Grid d_grid = Grid(NUMBER_OF_FISHES, h_options.radiusForFishes, WIDTH, HEIGHT, true);
+	Grid h_grid = Grid(numberOfFishes, h_options.radiusForFishes, WIDTH, HEIGHT, false);
+	Grid d_grid = Grid(numberOfFishes, h_options.radiusForFishes, WIDTH, HEIGHT, true);
 	h_grid.h_InitialiseArraysIndicesAndFishes();
 	d_grid.d_InitialiseArraysIndicesAndFishes(h_grid.indices);
 
 
 
-	dim3 numBlocks((NUMBER_OF_FISHES + THREAD_NUMBER - 1) / THREAD_NUMBER);
+	dim3 numBlocks((numberOfFishes + THREAD_NUMBER - 1) / THREAD_NUMBER);
 
 	cudaEvent_t start_main, stop_main;
 	cudaEventCreate(&start_main);
@@ -259,11 +266,24 @@ int main()
 
 		valuesChanged |= ImGui::SliderFloat("Force for wall avoidance", &h_options.forceForWallAvoidance, 0.1f, 0.5f);
 		valuesChanged |= ImGui::SliderFloat("Range to border", &h_options.rangeToBorderToStartTurn, 50.0f, 200.0f);
-
-		if (ImGui::Button("Reset to Defaults")) {
+		valuesChanged |= ImGui::SliderFloat("Fear of cursor", &h_options.powerOfFearForCursor, 0.01f, 0.1f, "%.3f");
+		ImGui::BeginDisabled();
+		ImGui::Checkbox("GPU", &withGpu);
+		ImGui::EndDisabled();
+		if (ImGui::Button("Switch"))
+		{
+			withGpuChanged = true;
+		}
+		ImGui::Checkbox("Drawing of circles", &circleDrawing);
+		ImGui::Checkbox("Cursor interaction", &fearingWithCursor);
+		if (ImGui::Button("Reset to Defaults"))
+		{
 			h_options.resetToDefaults();
 			valuesChanged = true;
 		}
+		float fps = ImGui::GetIO().Framerate;
+		ImGui::Text("FPS: %.1f", fps);
+
 
 		if (valuesChanged)
 		{
@@ -304,7 +324,7 @@ int main()
 			d_grid.SortCellsWithFishes();
 			d_grid.FindStartsAndEnds();
 			// Count for every fish the next position and velocity
-			CountForFishesGpu << <numBlocks, THREAD_NUMBER >> > (d_grid, d_options, d_fishes, d_trianglesVertices, NUMBER_OF_FISHES,
+			CountForFishesGpu << <numBlocks, THREAD_NUMBER >> > (d_grid, d_options, d_fishes, d_trianglesVertices, numberOfFishes,
 				cursorPosX, cursorPosY, fearingWithCursor);
 			checkCudaErrors(cudaGetLastError());
 			checkCudaErrors(cudaDeviceSynchronize());
@@ -314,8 +334,7 @@ int main()
 			float milliseconds = 0.0f;
 			cudaEventSynchronize(stop_main);
 			cudaEventElapsedTime(&milliseconds, start_main, stop_main);
-			printf("%f milliseconds for a frame\n", milliseconds);
-
+			ImGui::Text("milliseconds for main kernel: %.4f", milliseconds);
 			checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_vbo_res_triangles));
 
 			// Main drawing
@@ -329,7 +348,7 @@ int main()
 
 
 			glBindVertexArray(VAO_Triangles_GPU);
-			glDrawArrays(GL_TRIANGLES, 0, NUMBER_OF_FISHES * 3);
+			glDrawArrays(GL_TRIANGLES, 0, numberOfFishes * 3);
 
 			// Optional drawing of circles around
 			if (circleDrawing)
@@ -339,7 +358,7 @@ int main()
 
 				cudaEventRecord(start_circles);
 				CountCircleForFishesGpu << <numBlocks, THREAD_NUMBER >> > (d_fishes, d_circlesVertices,
-					NUMBER_OF_FISHES, NUMBER_OF_POINTS_FOR_CIRCLE, h_options.radiusForFishes);
+					numberOfFishes, NUMBER_OF_POINTS_FOR_CIRCLE, h_options.radiusForFishes);
 
 				checkCudaErrors(cudaGetLastError());
 				checkCudaErrors(cudaDeviceSynchronize());
@@ -347,27 +366,31 @@ int main()
 				float milliseconds_circle = 0.0f;
 				cudaEventSynchronize(stop_circles);
 				cudaEventElapsedTime(&milliseconds_circle, start_circles, stop_circles);
-				printf("%f milliseconds for the circles\n", milliseconds_circle);
+				ImGui::Text("milliseconds for circles generation: %.4f", milliseconds_circle);
 				checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_vbo_res_circle));
 
 				glUniform4f(colorPosition, 1.0f, 1.0f, 1.0f, 1.0f);
 				glBindVertexArray(VAO_Circles_GPU);
-				glMultiDrawArrays(GL_LINE_LOOP, firsts, count, NUMBER_OF_FISHES);
+				glMultiDrawArrays(GL_LINE_LOOP, firsts, count, numberOfFishes);
 			}
 
 		}
 		else
 		{
+			double startOfFunction = glfwGetTime();
 			h_grid.FindCellsForFishes(h_fishes);
 			h_grid.SortCellsWithFishes();
 			h_grid.FindStartsAndEnds();
 
-			CountForFishesCpu(h_grid, h_options, h_fishes, h_triangles_buffer, NUMBER_OF_FISHES, cursorPosX, cursorPosY,
+			CountForFishesCpu(h_grid, h_options, h_fishes, h_triangles_buffer, numberOfFishes, cursorPosX, cursorPosY,
 				fearingWithCursor);
 
 			h_grid.CleanStartsAndEnds();
 			h_grid.CleanAfterAllCount(h_fishes);
 
+			double endOfFunction = glfwGetTime();
+			double elapsedTime = (endOfFunction - startOfFunction) * 1000;
+			ImGui::Text("milliseconds for main function: %.4f", elapsedTime);
 			int screenWidth = glGetUniformLocation(shaderProgram, "width");
 			int screenHeight = glGetUniformLocation(shaderProgram, "height");
 			int colorPosition = glGetUniformLocation(shaderProgram, "color");
@@ -379,23 +402,26 @@ int main()
 
 			glBindBuffer(GL_ARRAY_BUFFER, VBO_Triangles_CPU);
 			glBindVertexArray(VAO_Triangles_CPU);
-			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float) * 6 * NUMBER_OF_FISHES, h_triangles_buffer);
-			glDrawArrays(GL_TRIANGLES, 0, NUMBER_OF_FISHES * 3);
+			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float) * 6 * numberOfFishes, h_triangles_buffer);
+			glDrawArrays(GL_TRIANGLES, 0, numberOfFishes * 3);
 
 
 			if (circleDrawing)
 			{
-				CountCircleForFishesCpu(h_fishes, h_circles_buffer, NUMBER_OF_FISHES, NUMBER_OF_POINTS_FOR_CIRCLE, h_options.radiusForFishes);
+				double startOfCirclesGeneration = glfwGetTime();
+				CountCircleForFishesCpu(h_fishes, h_circles_buffer, numberOfFishes, NUMBER_OF_POINTS_FOR_CIRCLE, h_options.radiusForFishes);
+				double endOfCirclesGeneration = glfwGetTime();
+				elapsedTime = (endOfCirclesGeneration - startOfCirclesGeneration) * 1000;
+				ImGui::Text("milliseconds for circles generation: %.4f", elapsedTime);
 				glUniform4f(colorPosition, 1.0f, 1.0f, 1.0f, 1.0f);
 				glBindBuffer(GL_ARRAY_BUFFER, VBO_Circles_CPU);
-				glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * NUMBER_OF_FISHES, h_circles_buffer);
+				glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float) * NUMBER_OF_POINTS_FOR_CIRCLE * 2 * numberOfFishes, h_circles_buffer);
 				glBindVertexArray(VAO_Circles_CPU);
-				glMultiDrawArrays(GL_LINE_LOOP, firsts, count, NUMBER_OF_FISHES);
+				glMultiDrawArrays(GL_LINE_LOOP, firsts, count, numberOfFishes);
 			}
 		}
 
-		
-
+	
 		// Render with opengl
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -406,6 +432,9 @@ int main()
 	cudaEventDestroy(start_main);
 	cudaEventDestroy(stop_main);
 	cudaEventDestroy(start_circles);
+
+	free(firsts);
+	free(count);
 
 	d_fishes.d_CleanMemoryForFishes();
 	h_fishes.h_CleanMemoryForFishes();

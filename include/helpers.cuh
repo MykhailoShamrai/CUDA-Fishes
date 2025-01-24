dd2c015dc9464d766e86e51de82ba67d6cb664cd
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-01-24 02:11:35 +0100

    I hope last version of a program

---
+++ include/helpers.cuh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <cuda_runtime.h>
+#define checkCudaErrors(call)                                \
+    do {                                                     \
+        cudaError_t err = call;                              \
+        if (err != cudaSuccess) {                            \
+            printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, \
+                   cudaGetErrorString(err));                 \
+            exit(EXIT_FAILURE);                              \
+        }                                                    \
+    } while (0)  
+
+__host__ __device__ float rand_float(float min, float max);
+
